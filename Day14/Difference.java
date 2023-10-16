97bfe1a5a3d2b51ad6649e0b3b816e8f6f60f51d
Chitwan Singh <110717037+Chitwan23@users.noreply.github.com>
2023-10-16 20:57:38 +0530

    Create Difference.java

---
+++ Day14/Difference.java
@@ -0,0 +1,11 @@
+
+
+ public Difference(int[] nums) {
+        elements = nums;
+    }
+
+    public void computeDifference() {
+        Arrays.sort(elements);
+        maximumDifference = elements[elements.length - 1] - elements[0];
+    }
+
