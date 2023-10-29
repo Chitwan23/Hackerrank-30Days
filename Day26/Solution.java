1d7c3090f2557104b9d7b5d88055aa508f08b7b2
Chitwan Singh <110717037+Chitwan23@users.noreply.github.com>
2023-10-29 23:51:56 +0530

    Solution.java

--- Day26/Solution.java
+++ Day26/Solution.java
@@ -1,41 +1,37 @@
-import java.io.*;
-import java.util.*;
-import java.text.*;
-import java.math.*;
-import java.util.regex.*;
+import java.util.Scanner;
 
-public class Solution {
+public class LibraryFine {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
+        // Read the actual return date.
+        int returnDay = scanner.nextInt();
+        int returnMonth = scanner.nextInt();
+        int returnYear = scanner.nextInt();
 
-        public static void main(String[] args) {
-        int fine;
-        
-        Scanner scan = new Scanner(System.in);
-        
-        int returnDay = scan.nextInt();
-        int returnMonth = scan.nextInt();
-        int returnYear = scan.nextInt();
-        
-        int dueDay = scan.nextInt();
-        int dueMonth = scan.nextInt();
-        int dueYear = scan.nextInt();
-        
-        scan.close();
-        
-        if(returnYear <= dueYear){
-            if(returnMonth <= dueMonth){
-                if(returnDay <= dueDay){
-                    fine = 0;
-                }else{
-                    fine = (returnDay - dueDay) * 15;
-                }
-            }else{
-                fine = (returnMonth - dueMonth) * 500;
-            }
-        }else{
+        // Read the due date.
+        int dueDay = scanner.nextInt();
+        int dueMonth = scanner.nextInt();
+        int dueYear = scanner.nextInt();
+
+        scanner.close();
+
+        int fine = 0;  // Initialize the fine to 0.
+
+        if (returnYear > dueYear) {
+            // Book is returned after the expected year, so fixed fine.
             fine = 10000;
+        } else if (returnYear == dueYear) {
+            // Same year, check for the month.
+            if (returnMonth > dueMonth) {
+                // Book is returned after the expected month but in the same year.
+                fine = (returnMonth - dueMonth) * 500;
+            } else if (returnMonth == dueMonth && returnDay > dueDay) {
+                // Book is returned after the expected day in the same month and year.
+                fine = (returnDay - dueDay) * 15;
+            }
         }
-        
+
         System.out.println(fine);
     }
 }
